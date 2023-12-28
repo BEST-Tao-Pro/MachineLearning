448978c96002249d6963fd712360365a2f58a38b
BEST-Tao-Pro <2569514965@qq.com>
2023-12-29 01:46:53 +0800

    Changes

--- 1.0线性算法.py
+++ 1.0线性算法.py
@@ -32,32 +32,3 @@ plt.xlabel('w')
 plt.ylabel('LOSS')
 plt.show()
 
-w = 1.0
-
-
-def forward(x):
-    return x * w
-
-
-def cost(xs, ys):
-    cost = 0
-    for x, y in zip(xs, ys):
-        y_pred = forward(x)
-        cost += (y_pred - y) ** 2
-        return cost / len(xs)
-
-
-def gradient(xs, ys):
-    grad = 0
-    for x, y in zip(xs, ys):
-        grad += 2 * x * (x * w - y)
-        return grad / len(xs)
-
-
-print('prediction:', 4, forward(4))
-for epoch in range(100):
-    cost_val = cost(x_data, y_data)
-    grad_val = gradient(x_data, y_data)
-    w -= 0.01 * grad_val
-    print('epoch:', epoch, 'w=', w, 'loss=', cost_val)
-print('prediction:', 4, forward(4))

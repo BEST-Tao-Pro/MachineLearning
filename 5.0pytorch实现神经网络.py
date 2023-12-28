ce0248d4d8e2f6c2126f61fe1861a09cc2b73f1c
BEST-Tao-Pro <2569514965@qq.com>
2023-12-29 01:44:49 +0800

    Changes

--- 5.0pytorch实现神经网络.py
+++ 5.0pytorch实现神经网络.py
@@ -1,11 +1,11 @@
 import torch
-from torch import nn
+
 
 x_data = torch.Tensor([[1.0], [2.0], [3.0]])
 y_data = torch.Tensor([[2.0], [4.0], [6.0]])
 
 
-class LinearModel(nn.Module):
+class LinearModel(torch.nn.Module):
     def __init__(self):
         super(LinearModel, self).__init__()
         self.linear = torch.nn.Linear(1, 1)
@@ -16,3 +16,18 @@ class LinearModel(nn.Module):
 
 
 model = LinearModel()
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in range(1000):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+print('w=', model.linear.weight.item())
+print('b=', model.linear.bias.item())
+x_test = torch.Tensor([[4.0]])
+y_test = model(x_test)
+print('y_pred=', y_test.data)
